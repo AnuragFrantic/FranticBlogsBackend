e9580f4272aab88f128a3bfaf0263891c31525e5
AnuragFrantic <anurag@franticpro.com>
2026-01-23 12:18:46 +0530

    `Added two new allowed origins to the list`

--- frantic.js
+++ frantic.js
@@ -92,6 +92,8 @@ const port = process.env.PORT || 5000;
 const allowedOrigins = [
     "http://localhost:3000",
     "https://frantic-blog.vercel.app",
+    "https://www.frantic.in/",
+    "https://frantic.in/"
     // Add your custom domain when you connect it:
     // "https://blogs.frantic.in",
 ];
